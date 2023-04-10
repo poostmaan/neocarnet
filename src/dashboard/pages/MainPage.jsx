0c9bb89739b2e42f05f5782b6a0b268c2fd69e4b
poostmaan <poostmaan@gmail.com>
2023-04-10 15:43:10 -0400

    Agregada seccion persons

---
+++ src/dashboard/pages/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { DashboardLayout } from '../layouts'
+
+export const MainPage = () => {
+  return (
+    <DashboardLayout>
+      <div>MainPage</div>
+    </DashboardLayout>
+  )
+}
