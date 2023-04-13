9344778f939f7b4a718e31e2a53fda5e4d72ab3a
poostmaan <poostmaan@gmail.com>
2023-04-13 10:33:48 -0400

    Modifictions profiles modules

--- src/theme/defaultTheme.js
+++ src/theme/defaultTheme.js
@@ -2,16 +2,20 @@ import { red } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
 
 export const defaultTheme = createTheme({
-    palette: {
-        primary: {
-            main: '#0205a1e8'
-        },
-        secondary: {
-            main: '#1acc8d'
-        },
-        error: {
-            main: red.A400
-        }
-    }
-})
-
+  palette: {
+    primary: {
+      main: "#0205a1e8",
+    },
+    secondary: {
+      main: "#1acc8d",
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+  },
+});
