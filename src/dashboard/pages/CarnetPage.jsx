cb1e6e9278611c0a3c61f79c21bb89fd7faee395
poostmaan <poostmaan@gmail.com>
2023-06-23 17:04:18 -0400

    cambiar texto

---
+++ src/dashboard/pages/CarnetPage.jsx
@@ -0,0 +1,119 @@
+import { DashboardLayout } from "../layouts"
+import { fabric } from "fabric";
+import { FabricJSCanvas, useFabricJSEditor } from 'fabricjs-react'
+import { useEffect, useRef } from "react";
+import { Box, Grid,  Chip, Button, Typography } from "@mui/material";
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import StayPrimaryLandscapeIcon from '@mui/icons-material/StayPrimaryLandscape';
+import StayPrimaryPortraitIcon from '@mui/icons-material/StayPrimaryPortrait';
+import {initCanvas} from './miscript'
+
+export const CarnetPage = () => {
+
+  const fileInputRef = useRef();
+
+  const handleDelete = () => {
+    console.info('You clicked the delete icon.');
+  };
+
+  useEffect(() => {
+    initCanvas()
+  }, []);
+  
+
+  return (
+    <DashboardLayout>
+      <div>Aqui podras crear, eliminar y editar los carnets</div>
+      {/* <button onClick={onAddCircle}>Add circle</button>
+      <button onClick={onAddRectangle}>Add Rectangle</button> */}
+      <Grid container>
+        {/* <Grid item md={2}>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: "column",  
+            justifyContent: "center",
+            alignItems: "center",
+            border: "1px solid #ddd",
+            mb: 2,
+            py:4
+          }}>
+            <span>Vertical</span>
+            <StayPrimaryPortraitIcon />  
+          </Box>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: "column",  
+            justifyContent: "center",
+            alignItems: "center",
+            border: "1px solid #ddd",
+            py: 4
+          }}>
+            <span>Horizontal</span>
+            <StayPrimaryLandscapeIcon /> 
+          </Box>
+        </Grid> */}
+        <Grid item md={9}>
+
+          <Box sx={{
+            display: 'flex',
+            flexDirection: "row",  
+            justifyContent: "center",
+            backgroundColor: "#ddd",
+            mx: 2,
+          }}>
+            <button id="bold">Bold</button>
+            <button id="italic">Italic</button>
+            <button id="underline">Underline</button>
+            <select id="fontsize">
+              <option value="">Tamaño de letra</option>
+              <option value="8">8px</option>
+              <option value="10">10px</option>
+              <option value="12">12px</option>
+              <option value="14">14px</option>
+              <option value="16">16px</option>
+              <option value="18">18px</option>
+              <option value="20">20px</option>
+              <option value="24">24px</option>
+              <option value="28">28px</option>
+              <option value="32">32px</option>
+              <option value="36">36px</option>
+              <option value="40">40px</option>
+              <option value="48">48px</option>
+              <option value="56">56px</option>
+              <option value="64">64px</option>
+            </select>
+            <Box sx={{
+              height: "500px",
+              width: "318px",
+              backgroundColor: "#ccc",
+              borderRadius: '20px',
+            }}>
+              {/* <FabricJSCanvas className="sample-canvas" onReady={onReady} /> */}
+              <canvas id="design" width="318" height="500"></canvas>
+            </Box>
+          </Box>
+
+        </Grid>
+        <Grid item md={3}>
+          <input type="file" name="subirFondo" id="subirFondo" style={{ display: 'none'}} ref={fileInputRef}/>
+          <Typography>Sugerencia de fondos: </Typography>
+          <Box sx={{
+            display: 'flex', 
+            mb: 2,
+          }}>
+            {imageTemplates.map(img => <img src={img} key={img} width="65" height="95" alt="image"/>)}
+          </Box>
+          <Button id="addBackgroundbtn" onClick={ ()=>{ fileInputRef.current.click() }} variant="outlined" fullWidth sx={{ mb: 2}}>Agregar fondo personalizado</Button>
+          <Chip label="cedula" onDelete={handleDelete} />
+          <Chip label="nombre" onDelete={handleDelete} />
+          <Chip label="apellido" onDelete={handleDelete} />
+          <Chip label="nrereferencia" onDelete={handleDelete} />
+          <Button id="toSvgBtn" variant="outlined" fullWidth>Guardar</Button>
+        </Grid>
+      </Grid>
+
+    </DashboardLayout>
+  )
+}
+
+const imageTemplates = ['https://neocarnets.neoaplicaciones.com/assets/bussinessTemplates/fvb.png', 'https://neocarnets.neoaplicaciones.com/assets/bussinessTemplates/asturiano.png']
---
+++ src/dashboard/pages/miscript.js
@@ -0,0 +1,145 @@
+function initCanvas() {
+
+  var $ = function (id) {
+    return document.getElementById(id);
+  };
+
+  /**
+   * @param {texts} Array 
+   */
+  function addText(texts = []) {
+    let textsInstances = texts.map(text => new fabric.Text(text, { top: 100, id: text }));
+    textsInstances.forEach(textInstance => {
+      return textInstance.on('mousedown', function(options) {  
+        console.log('Hiciste clic en el objeto de texto', options);
+        activeText = options.target.id;
+        // Aquí puedes realizar acciones adicionales al hacer clic en el objeto
+      });
+    })
+
+    canvas.add(...textsInstances);
+
+
+    console.log(textsInstances);
+  }
+
+  function changeText(option) {
+
+    if(activeText === '') {
+      console.log('no hay nada seleccionado')
+    }
+
+    const obj = {
+      'bold': {'fontWeight': 'bold'},
+      'italic': {'fontStyle': 'italic'},
+      'underline': {'underline': true},
+      'fontSize': {},
+    }
+
+    newText = canvas.getItemById(activeText);
+
+    const prop = obj[option] || {}
+
+    newText.set(prop);  
+  }
+
+  let activeText = '';
+
+  var canvas = new fabric.Canvas("design");
+  fabric.Object.prototype.transparentCorners = false;
+
+  // TODO: DEBEN SER EXTRAIDOS DE LA BASE DE DATOS
+  const texts = addText(["nombre", "cedula", "accion"]);
+
+  var imgElement = document.getElementById("my-image");
+  var imgInstance = new fabric.Image(imgElement, {
+    left: 0,
+    top: 0,
+    angle: 0,
+    opacity: 0.85,
+  });
+  canvas.setBackgroundImage(imgInstance, canvas.renderAll.bind(canvas));
+
+  let addBackgroundbtn = document.getElementById("addBackgroundbtn");
+  let toSvgBtn = document.getElementById("toSvgBtn");
+
+  document.getElementById("subirFondo").addEventListener("change", function (e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function (f) {
+      var data = f.target.result;
+      fabric.Image.fromURL(data, function (img) {
+        // add background image
+        canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
+          scaleX: canvas.width / img.width,
+          scaleY: canvas.height / img.height,
+        });
+      });
+    };
+    reader.readAsDataURL(file);
+  });
+
+  const textOptions = {
+    bold: document.querySelector('#bold'),
+    italic: document.querySelector('#italic'),
+    underline: document.querySelector('#underline'),
+    fontSize: document.querySelector('#fontsize'),
+  }
+
+  for (let [key, value] of Object.entries(textOptions)) {
+    console.log(value)
+    value.addEventListener('click', changeText(key))
+  }
+
+
+  toSvgBtn.addEventListener("click", ()=> console.log(canvas.toSVG()))
+
+
+  // var group = $('group'),
+  //     ungroup = $('ungroup'),
+  //     multiselect = $('multiselect'),
+  //     addmore = $('addmore'),
+  //     discard = $('discard');
+
+  //     addmore.onclick = add;
+
+  //     multiselect.onclick = function() {
+  //       canvas.discardActiveObject();
+  //       var sel = new fabric.ActiveSelection(canvas.getObjects(), {
+  //         canvas: canvas,
+  //       });
+  //       canvas.setActiveObject(sel);
+  //       canvas.requestRenderAll();
+  //     }
+
+  //     group.onclick = function() {
+  //       if (!canvas.getActiveObject()) {
+  //         return;
+  //       }
+  //       if (canvas.getActiveObject().type !== 'activeSelection') {
+  //         return;
+  //       }
+  //       canvas.getActiveObject().toGroup();
+  //       canvas.requestRenderAll();
+  //     }
+
+  //     ungroup.onclick = function() {
+  //       if (!canvas.getActiveObject()) {
+  //         return;
+  //       }
+  //       if (canvas.getActiveObject().type !== 'group') {
+  //         return;
+  //       }
+  //       canvas.getActiveObject().toActiveSelection();
+  //       canvas.requestRenderAll();
+  //     }
+
+  //     discard.onclick = function() {
+  //       canvas.discardActiveObject();
+  //       canvas.requestRenderAll();
+  //     }
+
+  //         document.body.innerText =canvas.toSVG();
+}
+
+export { initCanvas };
